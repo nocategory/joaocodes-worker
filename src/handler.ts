bf42b94cae174d39cc0b073ffcf08be63055572a
João <joao@joao.codes>
2021-07-19 17:25:37 +0100

    feat: query prismic endpoint & return it

--- src/handler.ts
+++ src/handler.ts
@@ -1,3 +1,16 @@
-export async function handleRequest(request: Request): Promise<Response> {
-  return new Response(`request method: ${request.method}`)
+import * as prismic from '@prismicio/client'
+
+export async function handleRequest(): Promise<Response> {
+  const client = prismic.createClient(PRISMIC_ENDPOINT, {
+    accessToken: PRISMIC_TOKEN,
+    fetch,
+  })
+
+  const ref = await client.getRefByLabel('Master')
+  client.queryContentFromRef(ref.ref)
+
+  const data = await client.getByID(PRISMIC_ID)
+  return new Response(JSON.stringify(data.data), {
+    headers: { 'content-type': 'application/json;charset=UTF-8' },
+  })
 }
